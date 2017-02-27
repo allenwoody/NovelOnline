68a928fd54d3091df33b81919a20f0a3bb2272b7
allenwoody <allenwoody0410@gmail.com>
2017-02-27 17:33:00 +0800

    NovelController
    添加方法

--- src/com/angentle/novel/web/controller/novel/NovelController.java
+++ src/com/angentle/novel/web/controller/novel/NovelController.java
@@ -6,6 +6,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.angentle.novel.core.generic.GenericController;
@@ -26,9 +27,25 @@ public class NovelController extends GenericController{
 	* @return String    返回类型 
 	* @throws
 	 */
-	public String listNovel(ModelMap map){
+	@RequestMapping("/myNovles")
+	public String listMyNovel(ModelMap map){
 		List<Novel> novelList = this.novelService.selectNovels(this.getLoginUser().getUserId());
 		map.put("novelList", novelList);
-		return "/listNovel";
+		return "/novel/myNovles";
+	}
+	/**
+	 * 
+	* @Title: publishedChapter 
+	* @Description: 已发布的章节
+	* @param @param chapterId
+	* @param @param map
+	* @param @return    设定文件 
+	* @return String    返回类型 
+	* @throws
+	 */
+	@RequestMapping("/chapterManager/{chapterId}")
+	public String publishedChapter(@PathVariable() String chapterId, ModelMap map){
+		
+		return "/publishedChapter";
 	}
 }
